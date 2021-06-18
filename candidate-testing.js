98905f68cfd40ed31def1029d48523e5dd315051
Monica <85039987+mdedomen4@users.noreply.github.com>
2021-06-18 01:21:59 +0000

    Assignment 1: Part 1 Complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,31 +3,37 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
-
+// TODO 1.1b: Ask for candidate's name //
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
-}
+  candidateName = input.question("What is your name? ");
+  }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
-}
-
+  candidateAnswer = input.question(question);
+  }
+  
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if (candidateAnswer === correctAnswer){
+      console.log("Correct!");
+  } else {
+    console.log("Incorrect.");
+  }
 
+//debugging:
+//console.log(candidateAnswer);
+//console.log(correctAnswer);
 
   let grade;
   
@@ -38,9 +44,13 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+  console.log(`Welcome to the quiz, ${candidateName}!`);
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+
+  //debug message:
+  //console.log("end of program!");
 }
 
 // Don't write any code below this line //
@@ -56,3 +66,4 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
